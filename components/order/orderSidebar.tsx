bc6c1de709ef6d6f38da0f483329c163eb7458fb
Juan Cruz <juan_pineda_12@hotmail.es>
2025-10-26 16:29:16 +0100

    logo

--- components/order/orderSidebar.tsx
+++ components/order/orderSidebar.tsx
@@ -3,11 +3,15 @@ import CategorySlider from "./CategorySlider";
 export default async function OrderSidebar() {
   
   return (
-    <aside className="xl:w-72 xl:h-screen bg-white p-3 xl:p-5" >
+    <aside className="xl:w-72 xl:h-screen xl:bg-white p-3 xl:p-5 relative " >
         
-        <nav className="flex xl:flex-col fixed top-0 left-0 h-20 bg-orange-100 py-2 w-full xl:relative xl:h-auto xl:bg-white z-10">
+        <nav className="flex xl:flex-col fixed top-0 left-0 h-20 bg-orange-100 py-2 w-full xl:relative xl:h-auto xl:bg-white z-10 xl:order-2">
           <CategorySlider />
         </nav>
+
+        <div className="mt-24 flex items-center justify-center w-full  xl:absolute xl:bottom-0 xl:left-0 xl:mb-5 xl:order-1">
+          <img className=" w-64 xl:w-50" src="/aria.png" alt="" />
+        </div>
         
     </aside>
   )
