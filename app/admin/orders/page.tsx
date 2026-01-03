ea1457ff17fa818dd4bd24d6de7b129e7d9db002
Juan Cruz <juan_pineda_12@hotmail.es>
2026-01-03 09:06:41 +0100

    page orders simple swr

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,29 +1,38 @@
-'use client'
-import useSWR from "swr"
+"use client";
+
+import useSWR from "swr";
 import OrdersGrid from "@/components/order/OrdersGrid";
 import Heading from "@/components/ui/Heading";
 import { OrderWithProducts } from "@/src/types";
 
 export default function OrdersPage() {
-  const url = '/admin/orders/api'
-  const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
-  const {data, error, isLoading} = useSWR<OrderWithProducts[]>(url, fetcher, {
-    refreshInterval: 60000,
-    revalidateOnFocus: false
-  })
+  const url = "/admin/orders/api";
 
-  if(isLoading) {
+  const fetcher = () => fetch(url).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 1000,
+    revalidateOnFocus: false,
+  });
+
+  if (isLoading) {
     return (
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-orange-500"></div>
+      <div className="flex justify-center items-center h-full">
+        <div className="animate-spin rounded-full h-12 w-12 border-4 border-gray-300 border-t-gray-900" />
       </div>
-    )
+    );
   }
 
-  if(data) return (
+  if (error) {
+    return <p>Error al cargar órdenes</p>;
+  }
+
+  if (!data) return null;
+
+  return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <Heading>Administrar Órdenes</Heading>
       <OrdersGrid orders={data} />
     </div>
-  )
+  );
 }
